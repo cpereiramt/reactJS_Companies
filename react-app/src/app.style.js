60d9fc6bbbeb655761f78cbd0a16d534be2c6344
cpereiramt <cpereiramt@gmail.com>
2022-05-28 19:28:13 -0400

    fix: improving responsiveness

--- react-app/src/app.style.js
+++ react-app/src/app.style.js
@@ -5,10 +5,16 @@ export const WrapperDiv = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 500px;
+  height: 100vh;
   background: ${(props) => props.theme.accent};
-  margin: 10% auto;
-  width: 50%;
+  margin: auto;
+  width: 100%;
   padding: 10px;
   text-align: center;
+  @media (min-width: 800px) {
+    height: 500px;
+    background: ${(props) => props.theme.accent};
+    margin: 10% auto;
+    width: 50%;
+  }
 `;
