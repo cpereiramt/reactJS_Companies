370ab1123e58c6b560e2eb38c5f80e1cccdb8b17
cpereiramt <cpereiramt@gmail.com>
2022-05-28 09:18:06 -0400

    feat: adding first api fetch

--- react-app/src/pages/home/index.js
+++ react-app/src/pages/home/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllCompanies } from "../../services/companies";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const [companies, setCompanies] = useState([]);
+  useEffect(() => {
+    getAllCompanies().then((data) => setCompanies(data));
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Vatin</th>
+        </tr>
+      </thead>
+      <tbody>
+        {companies.map((company) => (
+          <tr key={company.vatin}>
+            <td>{company.name}</td>
+            <td>{company.vatin}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Home;
---
+++ react-app/src/services/companies.js
@@ -0,0 +1,6 @@
+const url_api_host = "http://localhost:3000";
+export const getAllCompanies = async () => {
+  const response = await fetch(`${url_api_host}/companies`);
+  const data = await response.json();
+  return data;
+};
